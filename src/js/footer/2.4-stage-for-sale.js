de0076b4886dfc566624e254fbb49aed94bf6cdc
artem <work.kuznecov.samara@yandex.ru>
2020-09-27 22:47:51 +0400

    upd

--- src/js/footer/2.4-stage-for-sale.js
+++ src/js/footer/2.4-stage-for-sale.js
@@ -3,9 +3,9 @@ $(function () {
 
     switch ($('#jsBtnNextStage').attr('data-position')) {
       case '1':
-        $('.cars__line').attr('data-translate', '1');
-        $('.cars__slider').attr('data-translate', '1');
-        $('.cars__descr-box').attr('data-translate', '1');
+        $('.cars__line').attr('data-translate', '2');
+        $('.cars__slider').attr('data-translate', '2');
+        $('.cars__descr-box').attr('data-translate', '2');
 
         $('#jsBtnNextStage').attr('data-position', '2');
         $('#jsSliderWhite').attr('data-position', '2');
@@ -13,9 +13,9 @@ $(function () {
         $('.cars__descr.2').removeClass('o-10');        
         break;
       case '2':
-        $('.cars__line').attr('data-translate', '2');
-        $('.cars__slider').attr('data-translate', '2');
-        $('.cars__descr-box').attr('data-translate', '2');
+        $('.cars__line').attr('data-translate', '3');
+        $('.cars__slider').attr('data-translate', '3');
+        $('.cars__descr-box').attr('data-translate', '3');
 
         $('#jsBtnNextStage').attr('data-position', '3');
         $('#jsSliderWhite').attr('data-position', '3');
@@ -23,9 +23,9 @@ $(function () {
         $('.cars__descr.3').removeClass('o-10');
         break;
       case '3':
-        $('.cars__line').attr('data-translate', '3');
-        $('.cars__slider').attr('data-translate', '3');
-        $('.cars__descr-box').attr('data-translate', '3');
+        $('.cars__line').attr('data-translate', '4');
+        $('.cars__slider').attr('data-translate', '4');
+        $('.cars__descr-box').attr('data-translate', '4');
 
         $('#jsBtnNextStage').attr('data-position', '4');
         $('#jsSliderWhite').attr('data-position', '4');
@@ -33,9 +33,9 @@ $(function () {
         $('.cars__descr.4').removeClass('o-10');
         break;
       case '4':
-        $('.cars__line').attr('data-translate', '4');
-        $('.cars__slider').attr('data-translate', '4');
-        $('.cars__descr-box').attr('data-translate', '4');
+        $('.cars__line').attr('data-translate', '1');
+        $('.cars__slider').attr('data-translate', '1');
+        $('.cars__descr-box').attr('data-translate', '1');
 
         $('#jsBtnNextStage').attr('data-position', '1');
         $('#jsSliderWhite').attr('data-position', '1');
